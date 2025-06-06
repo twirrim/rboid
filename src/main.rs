b11e9e438f89b4008d09d431838ed3cc17268d28
Paul Graydon <paulgraydon@hotmail.com>
2025-06-05 22:18:23 -0700

    Make visual range configurable

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,12 @@ struct Flags {
     max_speed: f32,
     #[argh(option, description = "min speed (float)", default = "0.5")]
     min_speed: f32,
+    #[argh(
+        option,
+        description = "visible range of boids (float)",
+        default = "20.0"
+    )]
+    visible_range: f32,
 }
 
 pub fn main() -> GameResult {
@@ -32,20 +38,20 @@ pub fn main() -> GameResult {
     // go for a 5% of width margin
     let margin = width * 0.005;
     let state = MainState::new(
-        args.boids,     // boid count
-        width,          // width
-        height,         // height
-        margin,         // margin
-        args.max_speed, // max_speed
-        args.min_speed, // min_speed
-        2.0,            // protected_range
-        20.0,           // visible_range
-        0.10,           // avoid_factor
-        0.05,           // matching_factor
-        0.0005,         // centering_factor
-        0.2,            // turn_factor
-        22.0,           // cell_size
-        3.0,            // draw_radius
+        args.boids,               // boid count
+        width,                    // width
+        height,                   // height
+        margin,                   // margin
+        args.max_speed,           // max_speed
+        args.min_speed,           // min_speed
+        2.0,                      // protected_range
+        args.visible_range,       // visible_range
+        0.10,                     // avoid_factor
+        0.05,                     // matching_factor
+        0.0005,                   // centering_factor
+        0.2,                      // turn_factor
+        args.visible_range * 1.1, // cell_size
+        3.0,                      // draw_radius
     )?;
     event::run(ctx, event_loop, state)
 }
